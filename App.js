a8ae4e5787f7cc006208ed6feca7951f3e2dbaed
Furkan <132396838+tyial@users.noreply.github.com>
2024-07-21 11:50:31 +0300

    Update CrashHandler

--- App.js
+++ App.js
@@ -5,4 +5,16 @@ const client = new Client({intents: 32767});
 require("./Utils/eventLoader.js")(client)
 require("./Utils/slashHandler.js")(client)
 
+// CrashHandler --------------------------------------------------------------
+process.on('unhandledRejection', (reason, p) => {
+    console.error(reason);
+});
+process.on("uncaughtException", (err, origin) => {
+    console.error(' [AntiCrash] :: Uncaught Exception/Catch');
+})
+process.on('uncaughtExceptionMonitor', (err, origin) => {
+    console.error(' [AntiCrash] :: Uncaught Exception/Catch (MONITOR)');
+});
+// CrashHandler --------------------------------------------------------------
+
 client.login(config.Token);
